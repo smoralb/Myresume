ab8bbcdbc412fbc36f37cd7c486d9de6522d62d5
smoralber <smoralber@gmail.com>
2025-01-18 21:07:54 +0100

    Make icon responsive

--- composeApp/src/wasmJsMain/kotlin/org/smb/resume/App.kt
+++ composeApp/src/wasmJsMain/kotlin/org/smb/resume/App.kt
@@ -4,13 +4,15 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalWindowInfo
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -18,18 +20,25 @@ import myresume.composeapp.generated.resources.Res
 import myresume.composeapp.generated.resources.ic_under_construction
 import org.jetbrains.compose.resources.painterResource
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun App() {
     MaterialTheme {
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Column(verticalArrangement = Arrangement.spacedBy(14.dp)) {
-                Image(
-                    modifier = Modifier.size(80.dp),
-                    painter = painterResource(resource = Res.drawable.ic_under_construction),
-                    contentDescription = null
-                )
-                Text(text = "Under construction", style = TextStyle(fontSize = 20.sp, color = Color.Black))
-            }
+        val containerSize = LocalWindowInfo.current.containerSize.width
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(14.dp)
+        ) {
+            Image(
+                modifier = Modifier.width((containerSize / 3).dp),
+                painter = painterResource(resource = Res.drawable.ic_under_construction),
+                contentDescription = null
+            )
+            Text(
+                text = "Under construction",
+                style = TextStyle(fontSize = 20.sp, color = Color.Black)
+            )
         }
     }
 }
