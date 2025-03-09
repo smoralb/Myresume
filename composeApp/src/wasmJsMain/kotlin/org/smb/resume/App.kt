71219638b809110736545df124aa7d270157ea4f
smoralber <smoralber@gmail.com>
2025-03-09 20:37:04 +0100

    Create basic structure for the UI and take some notes

--- composeApp/src/wasmJsMain/kotlin/org/smb/resume/App.kt
+++ composeApp/src/wasmJsMain/kotlin/org/smb/resume/App.kt
@@ -1,24 +1,45 @@
 package org.smb.resume
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import org.smb.resume.header.HeaderView
 import org.smb.resume.ui.theme.MyResumeTheme
 import org.smb.resume.ui.theme.Spacing
+import org.smb.resume.ui.theme.Typography
 
 
 @Composable
 fun App() {
     MyResumeTheme {
         Scaffold(
-            modifier = Modifier.padding(all = Spacing.spacingExtraLarge),
             topBar = {
                 HeaderView()
             },
-            content = {
-
+            content = { contentPadding ->
+                Column(
+                    modifier = Modifier.fillMaxWidth().padding(contentPadding),
+                    horizontalAlignment = Alignment.Start,
+                    verticalArrangement = Arrangement.spacedBy(Spacing.spacingExtraLarge)
+                ) {
+                    Text("Projects section", style = Typography().displayMedium)
+                    HorizontalDivider()
+                    Text("All the logos of the companies I have working on with a small description")
+                }
+            },
+            bottomBar = {
+                Surface(modifier = Modifier.fillMaxWidth(), color = Color.DarkGray) {
+                    Text(text = "Contact and social media", color = Color.White)
+                }
             }
         )
     }
