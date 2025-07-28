3161a54e63cc176f5e9634cc57734e6b604af86c
smoralber@gmail.com <smoralber@gmail.com>
2025-07-28 16:11:18 +0200

    Create extension function for resize ui

---
+++ composeApp/src/wasmJsMain/kotlin/org/smb/resume/AppScreen.kt
@@ -0,0 +1,11 @@
+package org.smb.resume
+
+import androidx.compose.runtime.Composable
+import org.smb.resume.ui.theme.MyResumeTheme
+
+@Composable
+fun AppScreen() {
+    MyResumeTheme {
+        DesktopView()
+    }
+}
